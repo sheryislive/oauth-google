cb7cd7a0d6bae9b017b139d0794929fa29f3e173
asynchronousJavascriptor <harshu8545@gmail.com>
2023-08-19 21:04:28 +0530

    fixed bug

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ passport.use(
     {
       clientID: process.env["GOOGLE_CLIENT_ID"],
       clientSecret: process.env["GOOGLE_CLIENT_SECRET"],
-      callbackURL: "/oauth2/redirect/google",
+      callbackURL: "https://pooja-authentication.onrender.com/oauth2/redirect/google",
       scope: ["email", "profile"],
     },
     async function verify(issuer, profile, cb) {
